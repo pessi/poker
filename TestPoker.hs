d3238ffec7058b061f62fef5c7ffa10c985d1ca2
Pekka Pessi <ppessi@nvidia.com>
2012-11-08 18:25:37 +0200

    Add 5-card hands for rankiong in TestPoker

--- TestPoker.hs
+++ TestPoker.hs
@@ -1,4 +1,5 @@
 import Poker
+import Data.List
 
 h a = Card Hearts a
 s a = Card Spades a
@@ -22,3 +23,67 @@ jhands = map (\ h -> j : h) hands
 
 results = map checkHand hands
 jresults = map checkHand jhands
+
+perms [] = [[]]
+perms xs = [ x:ps | x <- xs , ps <- perms ( xs \\ [x] ) ]
+
+-- subsets of size k
+combs 0 _ = [[]]
+combs _ [] = []
+combs k (x:xs)
+  | k > length xs + 1 = []
+  | otherwise = map (x:) (combs (k-1) xs) ++ combs k xs
+
+suits = [Hearts, Spades, Diamonds, Clubs]
+
+handify :: [Int] -> [Card]
+handify rs = map (\ (s, r) -> Card s r) $ zip (cycle suits) rs
+
+heartify :: [Int] -> [Card]
+heartify rs = map (Card Hearts) rs
+
+modRank :: Int -> Int
+modRank n
+  | n < 1 = n + 13
+  | n > 13 = n - 13
+  | otherwise = n
+
+l2h :: [Int]
+l2h = [2..13] ++ [1]
+
+{- there will be 7,462 distinct hands in 5-card poker -}
+ranking5 :: [[Card]]
+ranking5 = highCards ++
+       pairs ++
+       twoPairs ++
+       threeOfKinds ++
+       straights ++
+       flushes ++
+       fullHouses ++
+       straightFlushes ++
+       fourOfKinds ++
+       royalFlush
+  where
+    highs :: [[Int]]
+    highs = concat ([ map (n :) (init $ combs 4 [2..n-1]) | n <- [7..13]] ++
+                     [ map (1 :) (init $ tail $ combs 4 [2..13])])
+    highCards = map handify highs
+    pairs = map handify $ concat [ map ([n,n] ++) (combs 3 (l2h \\ [n])) | n <- l2h]
+    twoPairs = map handify $ concat $ concat
+               [[[[n,n,m,m,x] | x <- l2h \\ [n,m]]
+                       | m <- [2..modRank $ n - 1]]
+                       | n <- tail l2h]
+    threeOfKinds = map handify $ concat
+                   [ map ([n,n,n] ++) (combs 2 (l2h \\ [n])) | n <- l2h]
+    conts :: [[Int]]
+    conts = concat [[[n..n+4] | n <- [1..9]] ++ [[10,11,12,13,1]]]
+    straights = map handify conts
+    flushes = map heartify highs
+    fullHouses = map handify $ concat [[[n,n,n,m,m] | m <- l2h \\ [n]] | n <- l2h]
+    straightFlushes = map heartify $ init conts
+    fourOfKinds = map handify $ concat [[[n,n,n,n,m] | m <- l2h \\ [n]] | n <- l2h]
+    royalFlush = map heartify [[1, 13, 12, 11, 10]]
+
+group5 = map length $ group $ map checkHand ranking5
+expect5 :: [Int]
+expect5 = [1277,2860,858,858,10,1277,156,9,156,1]
